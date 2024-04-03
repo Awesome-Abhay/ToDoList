3fbd140dcb2688cabb92bd21fc8ce5adb53f8aa8
Abhay Agrawal <agrawalabhay45@gmail.com>
2024-04-03 20:24:54 +0530

    done

--- app.js
+++ app.js
@@ -1,51 +1,73 @@
-document.querySelector("input").addEventListener('keyup',(e)=>{
-    
-    if(e.key=="Enter"){
-        let value= document.querySelector("input").value.trim();
-        if(value.length!=0){
-            document.querySelector("input").value="";
+document.querySelector("input").addEventListener('keyup', (e) => {
+
+    if (e.key == "Enter") {
+        let value = document.querySelector("input").value.trim();
+        if (value.length != 0) {
+            document.querySelector("input").value = "";
             updateTodo(value);
         }
-    }   
+    }
 })
 
 
-let tasks=[];
-
-function updateTodo(value){
-    let div= document.createElement("div");
-    div.innerHTML=`
+let tasks = [];
+const taskCount=0;
+function updateTodo(value) {
+    let div = document.createElement("div");
+    div.innerHTML = `
         <p> ${value} </p>
         <i class="far fa-trash-alt"></i>
     `;
-    div.className="todos";
+    div.className = "todos";
     document.querySelector(".box").append(div);
-    tasks.push(div);
+    tasks.push(value);
+    // console.log(tasks);
     
+    saveToLocalStorage(tasks);
+
 }
 
-for(let task of tasks){
-    task.addEventListener("click",doneTask);
-}
-
-let box=document.querySelector(".box");
+let box = document.querySelector(".box");
 
 
-box.addEventListener("click",(e)=>{
-    if(e.target.className=="todos"){
+box.addEventListener("click", (e) => {
+    if (e.target.classList.contains("todos")) {
         doneTask(e.target);
-    }else if(e.target.className=="far fa-trash-alt"){
+    } else if (e.target.className == "far fa-trash-alt") {
+        
         removeTask(e.target);
+        
+        
     }
-} )
-function doneTask(target){
+})
+function doneTask(target) {
     target.classList.toggle("done");
-    
+
 }
-function removeTask(target){
+function removeTask(target) {
+    
+    let removedElement=target.parentElement.textContent;  
+      
+    let newTasks= tasks.filter((element)=>{
+        return JSON.stringify(element)!=JSON.stringify(removedElement);
+    })
+    console.log(newTasks);
+    
+    saveToLocalStorage(newTasks);
     target.parentElement.remove();
-    
+
+}
+function saveToLocalStorage(tasks) {
+    localStorage.setItem(`tasks`, JSON.stringify(tasks));
 }
 
+document.addEventListener("DOMContentLoaded", () => {
+    const storedTasks = localStorage.getItem("tasks") || "[]";
+    const retrievedTasks = JSON.parse(storedTasks);
+    retrievedTasks.forEach(element => {
+        updateTodo(element);
+    });
+});
 
 
+// localStorage.removeItem("tasks");
